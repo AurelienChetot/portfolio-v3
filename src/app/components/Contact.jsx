ba1ecbb7b482273257eef382343605f4541dd54b
AurelienChetot <aurelien.chetot@gmail.com>
2024-10-21 14:25:28 +0200

    add email.js

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -1,17 +1,42 @@
+import { useRef } from "react";
 import Image from "next/image";
+import emailjs from "emailjs-com"; // Importer EmailJS
 
 import User from "../assets/svg/user.svg";
 import Arroba from "../assets/svg/arroba.svg";
 import Message from "../assets/svg/message.svg";
 
 export default function Contact() {
+  const form = useRef(); // Référence au formulaire
+
+  const sendEmail = (e) => {
+    e.preventDefault(); // Empêcher le rechargement de la page
+
+    // Envoi de l'email via EmailJS
+    emailjs
+      .sendForm(
+        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
+        form.current,
+        process.env.NEXT_PUBLIC_EMAILJS_USER_ID
+      )
+      .then((result) => {
+        console.log("Email envoyé avec succès :", result.text);
+        alert("Votre message a été envoyé avec succès !");
+      })
+      .catch((error) => {
+        console.error("Erreur lors de l'envoi de l'email :", error);
+        alert("Une erreur s'est produite. Veuillez réessayer.");
+      });
+  };
+
   return (
     <section id="contact" className="section-contact">
       <div className="top-color-contact"></div>
       <div className="bottom-color-contact">
         <h6>Contact</h6>
         <p>N’hésitez pas à m’écrire</p>
-        <form aria-labelledby="contact-form">
+        <form ref={form} onSubmit={sendEmail} aria-labelledby="contact-form">
           <div className="flex-name-mail">
             <div className="icone-text-container">
               <Image src={User} alt="" width={35} height={35} />
@@ -20,7 +45,7 @@ export default function Contact() {
             <input
               type="text"
               id="name"
-              name="user_name"
+              name="from_name"
               required
               aria-required="true"
             />
@@ -33,7 +58,7 @@ export default function Contact() {
             <input
               type="email"
               id="mail"
-              name="user_mail"
+              name="user_email"
               required
               aria-required="true"
             />
@@ -45,7 +70,7 @@ export default function Contact() {
             </div>
             <textarea
               id="msg"
-              name="user_message"
+              name="message" // Correspond à {{message}} dans le template
               required
               aria-required="true"
             ></textarea>
