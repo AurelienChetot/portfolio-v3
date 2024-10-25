78f21f7a6f1c7417b486094f6e65c12fb8d4bc46
AurelienChetot <aurelien.chetot@gmail.com>
2024-10-25 17:27:30 +0200

    fix deploy route.js

--- src/app/api/formations/route.js
+++ src/app/api/formations/route.js
@@ -4,12 +4,6 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function GET(request) {
-  // Vérifie si la requete provient d'une source autorisée (à optimiser pour la sécuritée)
-  const referrer = request.headers.get("referer");
-  if (!referrer || !referrer.includes("http://localhost:3000/")) {
-    return new Response("Unauthorized", { status: 403 });
-  }
-
   try {
     const formations = await prisma.formation.findMany();
     return new Response(JSON.stringify(formations), {
@@ -17,6 +11,7 @@ export async function GET(request) {
       headers: { "Content-Type": "application/json" },
     });
   } catch (error) {
+    console.error("Error fetching formations:", error);
     return new Response(JSON.stringify({ error: "Something went wrong" }), {
       status: 500,
       headers: { "Content-Type": "application/json" },
--- src/app/api/projets/route.js
+++ src/app/api/projets/route.js
@@ -4,19 +4,14 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function GET(request) {
-  // Vérifier si la requête provient d'une source autorisée
-  const referrer = request.headers.get("referer");
-  if (!referrer || !referrer.includes("http://localhost:3000/")) {
-    return new Response("Unauthorized", { status: 403 });
-  }
-
   try {
-    const projets = await prisma.projet.findMany();
+    const projets = await prisma.formation.findMany();
     return new Response(JSON.stringify(projets), {
       status: 200,
       headers: { "Content-Type": "application/json" },
     });
   } catch (error) {
+    console.error("Error fetching projets:", error); // Ajoute un log pour le débogage
     return new Response(JSON.stringify({ error: "Something went wrong" }), {
       status: 500,
       headers: { "Content-Type": "application/json" },
